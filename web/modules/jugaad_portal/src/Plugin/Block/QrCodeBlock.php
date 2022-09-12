88860fef10160f0321ccc3386e9baf6ec722779c
M Andriansyah Nurcahya <4andriansyah@gmail.com>
2022-09-13 01:27:17 +0700

    update

--- web/modules/jugaad_portal/src/Plugin/Block/QrCodeBlock.php
+++ web/modules/jugaad_portal/src/Plugin/Block/QrCodeBlock.php
@@ -4,15 +4,11 @@ namespace Drupal\jugaad_portal\Plugin\Block;
 
 use Drupal\Core\Access\AccessResult;
 use Drupal\Core\Block\BlockBase;
-use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\File\FileSystemInterface;
 use Drupal\Core\Session\AccountInterface;
-use Endroid\QrCode\Builder\Builder;
 use Endroid\QrCode\Color\Color;
 use Endroid\QrCode\Encoding\Encoding;
-use Endroid\QrCode\ErrorCorrectionLevel\ErrorCorrectionLevelHigh;
 use Endroid\QrCode\ErrorCorrectionLevel\ErrorCorrectionLevelLow;
-use Endroid\QrCode\Label\Alignment\LabelAlignmentCenter;
-use Endroid\QrCode\Label\Font\NotoSans;
 use Endroid\QrCode\Label\Label;
 use Endroid\QrCode\Logo\Logo;
 use Endroid\QrCode\QrCode;
@@ -43,14 +39,15 @@ class QrCodeBlock extends BlockBase {
    * {@inheritdoc}
    */
   public function build() {
+    $host = \Drupal::request()->getSchemeAndHttpHost();
     $current_path = \Drupal::service('path.current')->getPath();
-    $default_scheme = \Drupal::config('system.file')->get('default_scheme');
-    $file_path = \Drupal::service('file_system')->realpath($default_scheme . '://');
+    $file_path = \Drupal::service('file_url_generator')
+      ->generate('public://qrcode.png');
 
     $writer = new PngWriter();
 
     // Create QR code
-    $qrCode = QrCode::create($current_path)
+    $qrCode = QrCode::create($host . $current_path)
       ->setEncoding(new Encoding('UTF-8'))
       ->setErrorCorrectionLevel(new ErrorCorrectionLevelLow())
       ->setSize(300)
@@ -59,25 +56,15 @@ class QrCodeBlock extends BlockBase {
       ->setForegroundColor(new Color(0, 0, 0))
       ->setBackgroundColor(new Color(255, 255, 255));
 
-    // Create generic logo
-    $logo = Logo::create($file_path.'/symfony.png')
-      ->setResizeToWidth(50);
+    $result = $writer->write($qrCode, null, null);
 
-    // Create generic label
-    $label = Label::create('Label')
-      ->setTextColor(new Color(255, 0, 0));
+    $resultString = $result->getString();
 
-    $result = $writer->write($qrCode, $logo, $label);
-
-    echo $result->getString();
-
-    // Save it to a file
-    $result->saveToFile($file_path.'/qrcode.png');
-
-    $dataUri = $result->getDataUri();
+    $file_system = \Drupal::service('file_system');
+    $file_system->saveData($resultString, 'public://qrcode.png', FileSystemInterface::EXISTS_REPLACE);
 
     $build['content'] = [
-      '#markup' => '<img src="' . $dataUri . '" alt="barcode"   />',
+      '#markup' => '<img src="' . $file_path->toString() . '" alt="barcode"   />',
     ];
     return $build;
   }
